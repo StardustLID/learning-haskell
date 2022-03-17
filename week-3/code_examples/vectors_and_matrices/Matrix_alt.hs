caeba3235fea7dc8500f9caedc98fb527973d369
StardustLID <60168779+StardustLID@users.noreply.github.com>
2022-03-17 11:57:35 +0100

    corrected matrix

--- week-3/code_examples/vectors_and_matrices/Matrix_alt.hs
+++ week-3/code_examples/vectors_and_matrices/Matrix_alt.hs
@@ -35,6 +35,20 @@ tr = transpose
 -- trace [[]] = 0
 -- trace x = [[if x == y then 1 else 0 | x <- [1..n]] | y <- [1..n]]
 
+-- Scalar (dot) product
+-- version 1: loop / accumulator
+skProd1 :: Vector -> Vector -> Int
+skProd1 xs ys = loop xs ys 0
+    where
+        loop (x:xs) (y:ys) p = loop xs ys (x*y+p)
+        loop _ _ p = p
+
+-- version 2: explicit recursion
+skProd2 :: Vector -> Vector -> Int
+skProd2 (x:xs) (y:ys) = x*y + skProd2 xs ys
+skProd2 _ _ = 0
+
+-- version 3: using library functions (abstracted control)
 twoComp :: (c -> d) -> (a -> b -> c) -> a -> b -> d
 f `twoComp` g = \x y -> f (g x y)
 
@@ -44,7 +58,9 @@ skProd v w = sum (zipWith (*) v w)
 skProduct = sum `twoComp` zipWith (*)
 
 vecMult :: Matrix -> Vector -> Vector
-vecMult m w = [skProd v w | v <- m]
+vecMult m w = [skProd v w | v <- tr m]
 
 matMult :: Matrix -> Matrix -> Matrix
 matMult m1 m2 = [vecMult m1 v | v <- m2]
+-- matMult [[4,61,0],[78,8,90],[59,11,-7]] [[31,-58,-10],[66,3,1],[49,25,1]]
+-- [[-4990,1317,-5150],[557,4061,263],[2205,3200,2243]]
